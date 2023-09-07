319fd2ad16ef7fd2e09dea234f26a987720aba3f
moersou <101804141+moersou@users.noreply.github.com>
2023-09-07 21:08:02 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: 'Notionic',
-  author: '左蓝',
+  author: 'rama',
   email: 'i@zuolan.me',
   link: 'https://notionic-three-rust.vercel.app/',
   newsletter: 'Notionic Weekly',
